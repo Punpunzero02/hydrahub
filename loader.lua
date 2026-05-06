fa618d66cb39190be5341e7fd460b229132f1489
Punpunzero02 <96716263+Punpunzero02@users.noreply.github.com>
2026-05-06 10:36:57 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+if getgenv().HydraLoaded then return end
+getgenv().HydraLoaded = true
+
+local HWID = tostring(game:GetService("RbxAnalyticsService"):GetClientId())
+
+local ok, res = pcall(function()
+    return game:HttpGet("https://hydra-checker.vercel.app/api/load?hwid=" .. HWID)
+end)
+
+if not ok or not res or res == "return" or #res < 10 then
+    return
+end
+
+loadstring(res)()
