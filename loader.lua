6f0a5cd91bed7b3da4fd7b20575ab1cd1074fee5
Punpunzero02 <96716263+Punpunzero02@users.noreply.github.com>
2026-05-28 09:05:26 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,38 @@
-if getgenv().HydraLoaded then return end
-getgenv().HydraLoaded = true
+if getenv().HydraLoaded then return end
+getenv().HydraLoaded = true
 
 local HWID = tostring(game:GetService("RbxAnalyticsService"):GetClientId())
 
+-- Deteksi trade world
+local isTradeWorld = false
+pcall(function()
+    local TW_Data = require(game:GetService("ReplicatedStorage"):WaitForChild("Data"):WaitForChild("TradeWorldData"))
+    isTradeWorld = TW_Data and game.PlaceId == TW_Data.PlaceId
+end)
+if not isTradeWorld then
+    pcall(function()
+        isTradeWorld = workspace:FindFirstChild("TradeWorld") ~= nil
+    end)
+end
+
+
+local loadUrl = ""
+if isTradeWorld then
+    
+    loadUrl = "https://hydra-checker.vercel.app/api/load-market?hwid=" .. HWID
+else
+   
+    loadUrl = "https://hydra-checker.vercel.app/api/load?hwid=" .. HWID
+end
+
 local ok, res = pcall(function()
-    return game:HttpGet("https://hydra-checker.vercel.app/api/load?hwid=" .. HWID)
+    return game:HttpGet(loadUrl)
 end)
 
 if not ok or not res or res == "return" or #res < 10 then
     return
 end
 
-
-task.wait(2) 
+task.wait(2)
 
 loadstring(res)()
